5e6f9630232f7a97942f97cea2f5edbb87420464
AshrfCode <ashrfasadi435@gmail.com>
2025-12-04 00:06:43 +0200

    added client module-info

--- BistroClient/src/module-info.java
+++ BistroClient/src/module-info.java
@@ -5,4 +5,14 @@
  * 
  */
 module BistroClient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.sql;
+    
+    opens client to javafx.graphics, javafx.fxml;          
+    opens clientgui to javafx.fxml;                       
+
+    exports client;
+    exports clientgui;
 }
